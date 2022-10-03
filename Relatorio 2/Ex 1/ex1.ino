19fc156163a7ffd3384df3265b1a0359bcb92a47
lucasdemartha <lucasribm@gmail.com>
2022-10-02 22:55:21 -0300

    rel 2

---
+++ Relatorio 2/Ex 1/ex1.ino
@@ -0,0 +1,51 @@
+#define BIT0 0b00000001
+#define BIT1 0b00000010
+#define BIT2 0b00000100
+#define BIT3 0b00001000
+#define BIT4 0b00010000
+#define BIT5 0b00100000
+#define BIT6 0b01000000
+#define BIT7 0b10000000
+
+
+ISR (PCINT0_vect)
+{
+	PORTB |= BIT4;
+  	_delay_ms(2000);
+  	PORTB &= ~(BIT4);
+}
+ISR (PCINT1_vect)
+{
+ 	PORTB |= BIT4;
+  	_delay_ms(2000);
+  	PORTB &= ~(BIT4);        
+
+}
+ISR (PCINT2_vect)
+{
+ 	PORTB |= BIT4;
+  	_delay_ms(2000);
+  	PORTB &= ~(BIT4);        
+
+}
+int main()
+{
+  DDRB = BIT4;
+  
+  PORTD |= BIT6+BIT2;
+  PORTC |= BIT4;
+  PORTB |= BIT2;
+  
+  PCICR |= BIT2 + BIT1 + BIT0;
+  PCMSK2 |= BIT6+BIT2;
+  PCMSK0 |= BIT2;
+  PCMSK1 |= BIT4;
+  
+  PORTB &= ~(BIT4); 
+  
+  sei();
+  
+  for(;;){
+  }
+  
+}
